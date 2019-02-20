d46bfc535d0be82e737819b6733a41b60ba144f7
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-02-20 15:46:06 +0100

    LPS-90273 Rename content page to standard page and content page template to standard page template

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutPageTemplateManagementToolbarDisplayContext.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutPageTemplateManagementToolbarDisplayContext.java
@@ -100,7 +100,8 @@ public class LayoutPageTemplateManagementToolbarDisplayContext
 							_getAddLayoutPageTemplateEntryURL());
 						dropdownItem.setHref("#");
 						dropdownItem.setLabel(
-							LanguageUtil.get(request, "content-page-template"));
+							LanguageUtil.get(
+								request, "standard-page-template"));
 					});
 
 				addPrimaryDropdownItem(
