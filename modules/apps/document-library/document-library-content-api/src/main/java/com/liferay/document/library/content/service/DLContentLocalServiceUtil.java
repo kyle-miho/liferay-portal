8490d72ac56c69078135d75647e433fcc76bc56d
kyle.miho <kyle.miho@liferay.com>
2019-11-12 14:50:13 -0800

    LPS-104539 Regen

--- modules/apps/document-library/document-library-content-api/src/main/java/com/liferay/document/library/content/service/DLContentLocalServiceUtil.java
+++ modules/apps/document-library/document-library-content-api/src/main/java/com/liferay/document/library/content/service/DLContentLocalServiceUtil.java
@@ -385,10 +385,6 @@ public class DLContentLocalServiceUtil {
 		return getService().hasContent(companyId, repositoryId, path, version);
 	}
 
-	public static java.io.InputStream openContentInputStream(long contentId) {
-		return getService().openContentInputStream(contentId);
-	}
-
 	public static java.io.InputStream openDataInputStream(long contentId) {
 		return getService().openDataInputStream(contentId);
 	}
