fd7e36beef98537bb499dd8fd6a0ef66153f09b5
kyle.miho <kyle.miho@liferay.com>
2020-01-22 14:32:15 -0800

    LRQA-55106 Add CompanyGenerator test module

---
+++ modules/test/company-generator/src/main/java/com/liferay/company/generator/CompanyGenerator.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.company.generator;
+
+import com.liferay.company.generator.configuration.CompanyGeneratorConfiguration;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.util.PortalInstances;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Kyle Miho
+ */
+@Component(
+	configurationPid = "com.liferay.company.generator.CompanyGeneratorConfiguration",
+	immediate = true, service = {}
+)
+public class CompanyGenerator {
+
+	@Activate
+	protected void activate(Map<String, Object> properties) throws Exception {
+		_companyGeneratorConfiguration = ConfigurableUtil.createConfigurable(
+			CompanyGeneratorConfiguration.class, properties);
+
+		if (Validator.isNull(
+				_companyGeneratorConfiguration.virtualHostName())) {
+
+			throw new Exception(
+				"Please set virtualHostName in CompanyGeneratorConfiguration");
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"Creating a company with virtualHostName = " +
+					_companyGeneratorConfiguration.virtualHostName());
+		}
+
+		try {
+			_oldCompanyId = CompanyThreadLocal.getCompanyId();
+
+			_company = _companyLocalService.addCompany(
+				_companyGeneratorConfiguration.virtualHostName(),
+				_companyGeneratorConfiguration.virtualHostName(), "liferay.com",
+				false, 0, true);
+
+			PortalInstances.initCompany(
+				new MockServletContext(), _company.getWebId());
+
+			if (_log.isInfoEnabled()) {
+				_log.info(_company);
+			}
+		}
+		catch (PortalException pe) {
+			_log.error(pe, pe);
+		}
+	}
+
+	@Deactivate
+	protected void deactivate(Map<String, Object> properties) {
+		if (_companyGeneratorConfiguration == null) {
+			_companyGeneratorConfiguration =
+				ConfigurableUtil.createConfigurable(
+					CompanyGeneratorConfiguration.class, properties);
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"Deleting company with virtualHostName = " +
+					_companyGeneratorConfiguration.virtualHostName());
+		}
+
+		try {
+			_companyLocalService.deleteCompany(_company);
+
+			if (_log.isInfoEnabled()) {
+				_log.info(_company);
+			}
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+		finally {
+			CompanyThreadLocal.setCompanyId(_oldCompanyId);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CompanyGenerator.class);
+
+	private Company _company;
+	private volatile CompanyGeneratorConfiguration
+		_companyGeneratorConfiguration;
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	private long _oldCompanyId;
+
+}
