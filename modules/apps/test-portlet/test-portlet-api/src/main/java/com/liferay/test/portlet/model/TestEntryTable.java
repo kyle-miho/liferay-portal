d18da32e0460e62f5ba4600f77fcdf6054ed8b48
kyle.miho <kyle.miho@liferay.com>
2020-08-19 16:24:30 -0700

    TEMP2

---
+++ modules/apps/test-portlet/test-portlet-api/src/main/java/com/liferay/test/portlet/model/TestEntryTable.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.test.portlet.model;
+
+import com.liferay.petra.sql.dsl.Column;
+import com.liferay.petra.sql.dsl.base.BaseTable;
+
+import java.sql.Types;
+
+import java.util.Date;
+
+/**
+ * The table class for the &quot;TestEntry&quot; database table.
+ *
+ * @author Brian Wing Shun Chan
+ * @see TestEntry
+ * @generated
+ */
+public class TestEntryTable extends BaseTable<TestEntryTable> {
+
+	public static final TestEntryTable INSTANCE = new TestEntryTable();
+
+	public final Column<TestEntryTable, Long> mvccVersion = createColumn(
+		"mvccVersion", Long.class, Types.BIGINT, Column.FLAG_NULLITY);
+	public final Column<TestEntryTable, Long> entryId = createColumn(
+		"entryId", Long.class, Types.BIGINT, Column.FLAG_PRIMARY);
+	public final Column<TestEntryTable, Long> companyId = createColumn(
+		"companyId", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+	public final Column<TestEntryTable, Long> userId = createColumn(
+		"userId", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+	public final Column<TestEntryTable, Date> createDate = createColumn(
+		"createDate", Date.class, Types.TIMESTAMP, Column.FLAG_DEFAULT);
+	public final Column<TestEntryTable, Date> modifiedDate = createColumn(
+		"modifiedDate", Date.class, Types.TIMESTAMP, Column.FLAG_DEFAULT);
+	public final Column<TestEntryTable, String> name = createColumn(
+		"name", String.class, Types.VARCHAR, Column.FLAG_DEFAULT);
+	public final Column<TestEntryTable, String> description = createColumn(
+		"description", String.class, Types.VARCHAR, Column.FLAG_DEFAULT);
+	public final Column<TestEntryTable, Integer> status = createColumn(
+		"status", Integer.class, Types.INTEGER, Column.FLAG_DEFAULT);
+
+	private TestEntryTable() {
+		super("TestEntry", TestEntryTable::new);
+	}
+
+}
